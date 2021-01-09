d90f3ac0e7e0d7339ab6b592ded4876b60fd0015
Dean Silfen <dean.silfen@gmail.com>
2021-01-09 12:50:51 -0500

    Get the dinosaur running at the chicken

--- DinsoaurChicken/GameScene.swift
+++ DinsoaurChicken/GameScene.swift
@@ -12,68 +12,64 @@ class GameScene: SKScene {
 
     private var chicken: SKSpriteNode?
     private var floor : SKSpriteNode?
+    private var dino : SKSpriteNode?
 
     override func sceneDidLoad() {
         super.sceneDidLoad()
-        self.chicken = SKSpriteNode(imageNamed: "chicken-drawing")
+        self.chicken = self.childNode(withName: "chicken") as? SKSpriteNode
         self.chicken?.size = CGSize(width: 100, height: 100)
-        if let chicken = chicken {
-            self.addChild(chicken)
-        }
-        self.chicken?.position = CGPoint(
-            x: self.frame.midX,
-            y: self.frame.midY
-        )
+        self.dino = self.childNode(withName: "dino") as? SKSpriteNode
+        self.dino?.xScale = -1
+
+
+        let dinoSequence = SKAction.sequence([
+            SKAction.moveTo(x: self.frame.minX, duration: 3),
+            SKAction.hide(),
+            SKAction.moveTo(x: self.frame.maxX, duration: 0),
+            SKAction.unhide()
+        ])
+        let dinoLoop = SKAction.repeatForever(dinoSequence)
+        dino?.run(dinoLoop)
         self.floor = self.childNode(withName: "floor") as? SKSpriteNode
 
+//        self.chicken?.position = .zero
         if let floor = self.floor {
             floor.physicsBody = SKPhysicsBody(
                 rectangleOf: floor.frame.size
             )
             floor.physicsBody?.isDynamic = false
-            floor.physicsBody?.categoryBitMask = 1
+            floor.physicsBody?.categoryBitMask = Masks.floor
         }
 
         if let chicken = self.chicken {
             chicken.physicsBody = SKPhysicsBody(
                 rectangleOf: chicken.frame.size
             )
-            chicken.physicsBody?.collisionBitMask = 1
+            chicken.physicsBody?.categoryBitMask = Masks.chicken
+            chicken.physicsBody?.collisionBitMask = Masks.floor | Masks.dino
+        }
+
+        if let dino = self.dino {
+            dino.physicsBody = SKPhysicsBody(
+                rectangleOf: dino.frame.size
+            )
+            dino.physicsBody?.categoryBitMask = Masks.chicken
+            dino.physicsBody?.collisionBitMask = Masks.floor | Masks.chicken
         }
 
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody?.categoryBitMask = Masks.floor
     }
 
     override func didMove(to view: SKView) {
         view.showsPhysics = true
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        self.chicken?.position = pos
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        self.chicken?.position = pos
-    }
-    
+
     func touchUp(atPoint pos : CGPoint) {
-        self.chicken?.position = pos
+        self.chicken?.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
---
+++ DinsoaurChicken/Masks.swift
@@ -0,0 +1,14 @@
+//
+//  Masks.swift
+//  DinsoaurChicken
+//
+//  Created by Dean Silfen on 1/9/21.
+//
+
+import UIKit
+
+struct Masks {
+    static let floor: UInt32 = 1 << 0
+    static let chicken: UInt32 = 1 << 1
+    static let dino: UInt32 = 1 << 2
+}
